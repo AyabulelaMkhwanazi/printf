340961bbf6ada13688b4133af15627d9ba6042cf
BotsheloXolileBanda <bxbanda@gmail.com>
2023-08-20 08:19:29 -0700

    Done

--- plntfpractice/charadd.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-#include "plntfheader.h"
-
-int charadd(char g)
-{
-	return (write(1, &g, 1));
-}
--- plntfpractice/plntfheader.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PLNTFHEADER_H_
-#define _PLNTFHEADER_H_
-
-int charadd(char g);
-char *intadd(int inpu);
-int _printf(const char *format, ...);
-void prntadd(char memstor[], int *memunit);
-#endif
